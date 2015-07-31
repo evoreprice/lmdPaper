bc516df65f6a794064f1af50d1c3aedc23178d16
Tom Harrop <twharrop@gmail.com>
2015-07-31 10:17:02 +0200

    download at and sl genomes from phytozome and make gtfs

---
+++ src/sh/downloadCompGenomes.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# catch email address (-e) and password (-p) for jgi passed to bash script
+# if the jgi password has special characters this script won't work
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-e )	shift
+				EMAIL=$1
+				;;
+		-p )	shift
+				PASSWORD=$1
+				;;
+		* )		echo "Bad input"
+				exit 1
+	esac
+	shift
+done
+
+# open phytozome session
+
+curl https://signon.jgi.doe.gov/signon/create --data-ascii \
+	login="$EMAIL"\&password="$PASSWORD" \
+	-b cookies -c cookies > /dev/null
+
+# arabidopsis
+
+genome_dir='data/at/genome'
+genome_url='http://genome.jgi.doe.gov/Athaliana/download/_JAMO/53112a1d49607a1be0055864/Athaliana_167_TAIR9.fa.gz'
+annot_url='http://genome.jgi.doe.gov/Athaliana/download/_JAMO/53112a1a49607a1be005585d/Athaliana_167_TAIR10.gene_exons.gff3.gz'
+
+genome_file="$(basename $genome_url .fa.gz)"
+annotation_file="$(basename $annot_url .gff3.gz)"
+
+# download genomes
+if [ ! -d $genome_dir ]; then
+	mkdir -p $genome_dir
+fi
+
+curl $genome_url -b cookies -c cookies > $genome_dir/$genome_file.fa.gz
+curl $annot_url -b cookies -c cookies > $genome_dir/$annotation_file.gff3.gz
+
+gunzip $genome_dir/$genome_file.fa.gz
+gunzip $genome_dir/$annotation_file.gff3.gz
+
+# make cuffcomp gtf
+cuffcompare -s $genome_dir/$genome_file.fa -CG -r $genome_dir/$annotation_file.gff3 \
+	-o $genome_dir/$annotation_file.cuffcomp $genome_dir/$annotation_file.gff3
+
+# stash new gtf
+mv $genome_dir/$annotation_file.cuffcomp.combined.gtf $genome_dir/gtf_final.tmp
+
+# remove cuffcomp intermediates
+rm $genome_dir/*cuffcomp*
+mv $genome_dir/gtf_final.tmp $genome_dir/$annotation_file.cuffcomp.gtf
+
+# log metadata
+cat -t <<- _EOF_ > $genome_dir/METADATA.csv
+	script,${0}
+	branch,$(git rev-parse --abbrev-ref HEAD)
+	hash,$(git rev-parse HEAD)
+	date,$(date +%F)
+	cuffcomp version,$(cuffcompare -h 2>&1 | head -n 1)
+_EOF_
+
+# tomato
+
+genome_dir='data/sl/genome'
+genome_url='http://genome.jgi.doe.gov/Slycopersicum/download/_JAMO/53112a8a49607a1be0055953/Slycopersicum_225_iTAGv2.40.fa.gz'
+annot_url='http://genome.jgi.doe.gov/Slycopersicum/download/_JAMO/53112a8a49607a1be0055952/Slycopersicum_225_iTAGv2.3.gene_exons.gff3.gz'
+
+genome_file="$(basename $genome_url .fa.gz)"
+annotation_file="$(basename $annot_url .gff3.gz)"
+
+# download genomes
+if [ ! -d $genome_dir ]; then
+	mkdir -p $genome_dir
+fi
+
+curl $genome_url -b cookies -c cookies > $genome_dir/$genome_file.fa.gz
+curl $annot_url -b cookies -c cookies > $genome_dir/$annotation_file.gff3.gz
+
+gunzip $genome_dir/$genome_file.fa.gz
+gunzip $genome_dir/$annotation_file.gff3.gz
+
+# make cuffcomp gtf
+cuffcompare -s $genome_dir/$genome_file.fa -CG -r $genome_dir/$annotation_file.gff3 \
+	-o $genome_dir/$annotation_file.cuffcomp $genome_dir/$annotation_file.gff3
+
+# stash new gtf
+mv $genome_dir/$annotation_file.cuffcomp.combined.gtf $genome_dir/gtf_final.tmp
+
+# remove cuffcomp intermediates
+rm $genome_dir/*cuffcomp*
+mv $genome_dir/gtf_final.tmp $genome_dir/$annotation_file.cuffcomp.gtf
+
+# tidy up
+
+rm cookies
+
+# log metadata
+cat -t <<- _EOF_ > $genome_dir/METADATA.csv
+	script,${0}
+	branch,$(git rev-parse --abbrev-ref HEAD)
+	hash,$(git rev-parse HEAD)
+	date,$(date +%F)
+	cuffcomp version,$(cuffcompare -h 2>&1 | head -n 1)
+_EOF_
+
+exit 0
