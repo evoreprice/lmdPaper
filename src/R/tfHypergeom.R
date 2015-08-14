8e5637e2303e398e92aa9ef802fbc7786080e88a
Tom Harrop <twharrop@gmail.com>
2015-08-14 16:23:14 +0200

    fix formatting on hypergeom table

--- src/R/tfHypergeom.R
+++ src/R/tfHypergeom.R
@@ -64,12 +64,12 @@ resultsLong$padj <- p.adjust(resultsLong$`p-value`, method = "BH",
 padj <- reshape2::dcast(resultsLong, Family ~ Cluster, value.var = 'padj')
 rownames(padj) <- padj$Family
 padj$Family <- NULL
-colnames(padj) <- paste0("padj(", colnames(padj), ")")
+colnames(padj) <- paste0("*p*~adj,\\ ", colnames(padj), "~")
 
 Number <- reshape2::dcast(resultsLong, Family ~ Cluster, value.var = 'Number')
 rownames(Number) <- Number$Family
 Number$Family <- NULL
-colnames(Number) <- paste0("n(", colnames(Number), ")")
+colnames(Number) <- paste0("*n*~", colnames(Number), "~")
 
 # remove columns that don't have p-values
 keep <- apply(padj, 1, function(x) !all(is.na(x)))
@@ -78,16 +78,19 @@ sigFamilies <- cbind(Number[keep, ], padj[keep,])
 
 # add the number of genes per family
 genesPerFamily <- sapply(tfdbByFamily, function(x) dim(x)[1])
-sigFamilies$`Number of genes` <- genesPerFamily[rownames(sigFamilies)]
+sigFamilies$`*n*` <- genesPerFamily[rownames(sigFamilies)]
 
 # add the number of expresed genes per family
-sigFamilies$`Number of expressed genes` <- exprPerFamily[rownames(sigFamilies)]
+sigFamilies$`*n*~expr~` <- exprPerFamily[rownames(sigFamilies)]
 
 # set the column order
-sigFamilies <- sigFamilies[,c('Number of genes', 'Number of expressed genes',
-                              'n(C1)', 'padj(C1)', 'n(C2)', 'padj(C2)', 'n(C3)',
-                              'padj(C3)', 'n(C4)', 'padj(C4)', 'n(C5)',
-                              'padj(C5)', 'n(C6)', 'padj(C6)')]
-
+#paste(colnames(sigFamilies)[c(13,14,1,7,2,8,3,9,4,10,5,11,6,12)], collapse = "', '")
+sigFamilies <- sigFamilies[,c('*n*', '*n*~expr~', '*n*~C1~', '*p*~adj,\\ C1~',
+                              '*n*~C2~', '*p*~adj,\\ C2~', '*n*~C3~',
+                              '*p*~adj,\\ C3~', '*n*~C4~', '*p*~adj,\\ C4~',
+                              '*n*~C5~', '*p*~adj,\\ C5~', '*n*~C6~',
+                              '*p*~adj,\\ C6~')]
+  
+  
 # output
 saveRDS(sigFamilies, paste0(mfuzzDir, "/sigFamilies.Rds"))
